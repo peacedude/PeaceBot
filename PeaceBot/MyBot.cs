212db1af89ee132d53c27ec62ec8d3f5ba37491a
peacedude <victor_sanden@hotmail.com>
2017-03-13 14:29:49 +0100

    Added 2 commands 1. Meme Command 2. Purge command (takes parameter of amount of messages to delete)

--- PeaceBot/MyBot.cs
+++ PeaceBot/MyBot.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
 
@@ -10,36 +6,88 @@ namespace PeaceBot
 {
     class MyBot
     {
-        private DiscordClient discord;
-        CommandService commands;
+        private readonly DiscordClient _discord;
+        private readonly CommandService _commands;
+
+        private Random rand;
+
+        private readonly string[] _freshestMemes;
 
         public MyBot()
         {
-            discord = new DiscordClient(x =>
+            rand = new Random();
+
+            _freshestMemes = new[]
             {
-                x.LogLevel = LogSeverity.Info;
+                "meme/meme1.jpg",
+                "meme/meme2.jpg",
+                "meme/meme3.jpg"
+            };
+
+            _discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Debug;
                 x.LogHandler = Log;
             });
 
-            discord.UsingCommands(x =>
+            _discord.UsingCommands(x =>
             {
-                x.PrefixChar = '#';
+                x.PrefixChar = '!';
                 x.AllowMentionPrefix = true;
             });
 
             
-            commands = discord.GetService<CommandService>();
+            _commands = _discord.GetService<CommandService>();
 
+            RegisterMemeCommand();
+            RegisterPurgeCommand();
 
-            discord.ExecuteAndWait(async () =>
+            _discord.ExecuteAndWait(async () =>
             {
-                await discord.Connect("MjkwODAzOTI0NTM2MDAwNTEy.C6gRvQ.eyMVt6U507LHn_T4hsgCxEkcXZc", TokenType.Bot);
+                await _discord.Connect("MjkwODAzOTI0NTM2MDAwNTEy.C6gRvQ.eyMVt6U507LHn_T4hsgCxEkcXZc", TokenType.Bot);
             });
         }
 
+        private void RegisterPurgeCommand()
+        {
+            _commands.CreateCommand("purge")
+                .Parameter("purgeAmount")
+                .Do(async (e) =>
+                {
+                    var amnt = 0;
+                    try
+                    {
+                        amnt = Convert.ToInt32(e.GetArg("purgeAmount"));
+                    }
+                    catch (FormatException)
+                    {
+                        await e.Channel.SendMessage("Invalid amount.");
+                    }
+                    if (amnt <= 100)
+                    {
+                        var messagesToDelete = await e.Channel.DownloadMessages(amnt);
+                        await e.Channel.DeleteMessages(messagesToDelete);
+                        await e.Channel.SendMessage(e.GetArg("purgeAmount") + " messages deleted.");
+                    }
+                    else if(amnt > 100)
+                    {
+                        await e.Channel.SendMessage("The maximum amount is 100.");
+                    }
+                });
+        }
 
+        private void RegisterMemeCommand()
+        {
+            _commands.CreateCommand("meme")
+                .Do(async (u) =>
+                {
+                    var randomMemeIndex = rand.Next(_freshestMemes.Length);
+                    var memeToPost = _freshestMemes[randomMemeIndex];
+                    await u.Channel.SendFile(memeToPost);
+                });
+        }
 
-        private void Log(object sender, LogMessageEventArgs e)
+        private static void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
         }
